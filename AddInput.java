467daa1a962ab8a9952e6e908a8da476bb21492d
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-01-25 08:55:40 -0600

    Update AddInput.java

--- AddInput.java
+++ AddInput.java
@@ -1,3 +1,5 @@
+//import java.util.ArrayList;
+
 public class AddInput { 
 
     public static void main(String[] args) {
@@ -7,19 +9,27 @@ public class AddInput {
         // The variable "args" holds all of the arguments that were passed into this program
         // The variable "args" is called an "array" because it's actually a list of items
         // If you good "How to get the number of items in an array in Java", you should be able to figure out how many integers there are in the variable "args"
-        // int num_args = ???
-        // if (??? != 2) {
+       // int num_args = args.size();
+       //  if (num_args != 2) {
+         //  System.out.println("Invalid Input");
+           
         //    do something
-        // }
+       //  }
         // More advanced: What if the two inputs aren't integers? Then what? How cold you check that?
     
         // **** START HERE: ****
         // These two lines will get you the first two arguments 
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
-        int sum = 
-        
+        int sum = a + b;
+        int num_args = args.length;
+        if (num_args != 2) {
+          System.out.println("Invalid Input");
+          System.exit(0);
+        }
+        else {
+          System.out.println("The sum of " + a + " and " + b + " is " + sum);
         // Print out "The sum of _ and _ is _" where you actually print the numbers they enter and the sum
-
+        }
     }
 }
